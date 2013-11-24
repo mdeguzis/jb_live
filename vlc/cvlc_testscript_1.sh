a09762394863e8845993e4a9e66782e2b8cab055
mikeyd <mikeyd@FedoraMTD.westell.com>
2013-11-24 14:05:36 -0500

    renamed vlc folder

---
+++ vlc/cvlc_testscript_1.sh
@@ -0,0 +1,2 @@
+cvlc http://videocdn-us.geocdn.scaleengine.net/jblive-iphonlive/jblive.stream/playlist.m3u8 --http-continuous --no-http-continuous --stop-time=50 --sout="#std{access=file,mux=mkv,dst='output2.mkv'}"
+
