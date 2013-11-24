40a21667da74caf268130f81654ce3df2bcb76b3
mikeyd <mikeyd@FedoraMTD.westell.com>
2013-11-24 14:04:53 -0500

    removed old vlc folder

--- VLC_Testing_Scripts/cvlc_testscript_1.sh
+++
@@ -1,2 +0,0 @@
-cvlc http://videocdn-us.geocdn.scaleengine.net/jblive-iphonlive/jblive.stream/playlist.m3u8 --http-continuous --no-http-continuous --stop-time=50 --sout="#std{access=file,mux=mkv,dst='output2.mkv'}"
-
