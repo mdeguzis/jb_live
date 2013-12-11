63c89638ec91b8796b6e852f7ab85ec63f204847
ProfessorKaos64 <mdeguzis@gmail.com>
2013-12-10 22:44:48 -0500

    corrected scibyte script filename

--- ffmpeg/jb_live_scibyte.sh
+++ ffmpeg/jb_live_scibyte.sh
@@ -3,6 +3,6 @@
 #Script to record JB Live shows
 #
 cd /home/$USER/Videos
-ffmpeg -f rtsp -rtsp_transport tcp -i rtsp://videocdn-us.geocdn.scaleengine.net/jblive/live/jblive.stream -strict -2 -vcodec copy -t 02:00:00 -y planb_`date +%Y%m%d`.avi
+ffmpeg -f rtsp -rtsp_transport tcp -i rtsp://videocdn-us.geocdn.scaleengine.net/jblive/live/jblive.stream -strict -2 -vcodec copy -t 02:00:00 -y scibyte_`date +%Y%m%d`.avi
 cd /home/$USER
 
